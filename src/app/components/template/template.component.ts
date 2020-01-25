0d861e27925cfc2db7a3de3263520c1e387f681d
Ariel Ezequiel Garrido <garridoar@outlook.com>
2020-01-25 02:47:53 -0300

    first commit

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar() {
+    console.log('Fomulario posteado');
+  }
+}
