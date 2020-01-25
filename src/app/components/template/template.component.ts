bb49204a7afffea1c84db6e146f941d6e3397795
Ariel Ezequiel Garrido <garridoar@outlook.com>
2020-01-25 19:57:48 -0300

    second commit

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,18 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: "",
+    sexo: "Hombre",
+    acepta: false
+  };
+
+  paises = [{
+    codigo: "CRI",
+    nombre: "Costa Rica"
+  },
+  {
+    codigo: "ESP",
+    nombre: "España"
+  }];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar() {
+  guardar( forma: NgForm ) {
     console.log('Fomulario posteado');
+    console.log("NgForm: ",forma);
+    console.log("Valor: ", forma.value);
   }
 }
